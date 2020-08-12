2ca34baf2af9f3ec98d9d1dcad8df3248b68b151
Kendy JEAN-MARIE <kendyjm@protonmail.com>
2020-08-12 16:35:27 +0200

    fix: convert urlExpiration from string to number

--- course-04/exercises/lesson-5-authentication/solution-kendyjm/src/lambda/http/createImage.ts
+++ course-04/exercises/lesson-5-authentication/solution-kendyjm/src/lambda/http/createImage.ts
@@ -13,7 +13,7 @@ const s3 = new AWS.S3({
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
 const bucketName = process.env.IMAGES_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+const urlExpiration: number = +process.env.SIGNED_URL_EXPIRATION
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Caller event', event)
